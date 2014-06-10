c073030fab393c70e552c1b4a5fddf80a5bba517
Clinton Blackburn <clinton.blackburn@gmail.com>
2014-06-09 21:59:12 -0400

    Removed Invalid Favicon Links

--- header.php
+++ header.php
@@ -39,16 +39,6 @@
         <meta property="og:description" content="<?php echo $description; ?>"/>
 				
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		
-		<!-- icons & favicons -->
-		<!-- For iPhone 4 -->
-		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/images/cb-logo-114x114.png">
-		<!-- For iPad 1-->
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/images/cb-logo-72x72.png">
-		<!-- For iPhone 3G, iPod Touch and Android -->
-		<link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/images/cb-logo-57x57.png">
-		<!-- For everything else -->
-		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/cb-logo.png">
 				
 		<!-- media-queries.js (fallback) -->
 		<!--[if lt IE 9]>
